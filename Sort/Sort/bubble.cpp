107ab771182e29166bd0df510b2e921abf6773fd
nicelbc <maillbc@qq.com>
2019-07-19 15:39:55 +0800

    Update bubble.cpp

--- Sort/Sort/bubble.cpp
+++ Sort/Sort/bubble.cpp
@@ -1,5 +1,5 @@
 void bubble(vector<int> &arr){
-for(int i =0;i < arr.size();i++)
+	for(int i =0;i < arr.size();i++)
         {
             for(int j = 0;j < arr.size() - i - 1;j++)// j开始等于0，
             {
